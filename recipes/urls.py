d515b51ca239605d75d2961024f70f42c47d33c9
Yeraldinnesan <ysanabria92@hotmail.com>
2023-04-11 01:20:47 -0500

    feat: :sparkles: search bar bug fixed

--- recipes/urls.py
+++ recipes/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name="home"),
     path('create-recipe', views.create_recipe, name="create-recipe"),
+    path('search-recipe', views.search_recipe, name="searchrecipe"),
     path('<int:recipe_id>', views.recipe_detail, name='recipedetail'),
 
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.shortcuts import render, get_object_or_404
 from .forms import RecipeForm
 from .models import Recipe
+from django.db.models import Q
+from django.urls import reverse
 
 # Create your views here.
 
@@ -25,7 +27,23 @@ def create_recipe(request):
             recipe = form.save(commit=False)
             recipe.category = form.cleaned_data['category']
             recipe.save()
-            return redirect('add_recipe_to_category', recipe_id=recipe.id)
+            return redirect(reverse('recipedetail', args=[recipe.id]))
     else:
         form = RecipeForm()
     return render(request, 'createrecipe.html', {'form': form})
+
+
+def search_recipe(request):
+    queryset = Recipe.objects.all()
+    query = request.GET.get('keywords')
+    category = request.GET.get('category')
+
+    if query:
+        queryset = queryset.filter(
+            Q(title__icontains=query) |
+            Q(description__icontains=query) |
+            Q(ingredients__icontains=query) |
+            Q(instructions__icontains=query)
+        ).distinct()
+
+    return render(request, 'searchresult.html', {'recipes': queryset})
